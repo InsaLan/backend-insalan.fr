9b0bc69cbb1e8f0dee86d131bece139daafa0707
Floris Bartra <floris.bartra@gmail.com>
2023-02-23 00:35:30 +0100

    Improved admin interface for the partners

--- insalan/partner/admin.py
+++ insalan/partner/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Partner
 
-admin.site.register(Partner)
+class PartnerAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'url', 'logo', 'type')
+    search_fields = ['name']
+
+
+admin.site.register(Partner, PartnerAdmin)
