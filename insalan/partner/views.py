6cd45cce5aeab0d06113bd3d8306f1e0f8824216
Floris Bartra <floris.bartra@gmail.com>
2023-04-24 20:29:44 +0200

    Added permissions in partner

--- insalan/partner/views.py
+++ insalan/partner/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Partner
 from .serializers import PartnerSerializer
@@ -7,8 +7,10 @@ from .serializers import PartnerSerializer
 class PartnerList(generics.ListCreateAPIView):
     queryset = Partner.objects.all().order_by('id')
     serializer_class = PartnerSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 
 class PartnerDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Partner.objects.all()
     serializer_class = PartnerSerializer
+    permission_classes = [permissions.IsAdminUser]
