7edeabf88bad37509158abd489f577da7fcaae2a
Mahal <teo.nespoulet@protonmail.com>
2023-07-29 20:54:02 +0200

    [unstable] changing user endpoint

--- insalan/user/models.py
+++ insalan/user/models.py
@@ -3,34 +3,47 @@ Module for the definition of models tied to users
 """
 
 from typing import Optional, List
-
+from datetime import date
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
 
-class User(AbstractUser):
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError(_('An email is required')) # TODO: translation?
+        if not username:
+            raise ValueError(_('An username is required'))
+        if not password:
+            raise ValueError(_('A password is required'))
+        user = self.model(
+                email=self.normalize_email(email),
+                username=username,
+                date_joined=date.today()
+                )
+        user.set_password(password)
+        user.save()
+        return user
+class User(AbstractUser, PermissionsMixin):
     """
     A user is simply our own abstraction defined above the standard Django User
     class.
     """
-
     def __init__(self, *args, **kwargs):
         AbstractUser.__init__(self, *args, **kwargs)
 
     USERNAME_FIELD = "username"
     EMAIL_FIELD = "email"
 
-    email = models.EmailField(
-        verbose_name="email address", max_length=255, unique=True, blank=False
-    )
-    email_active = models.BooleanField(verbose_name="Email Activated", default=False)
-    # Actually only used for super-user CLI tool
-    # REQUIRED_FIELDS = [
-    #         'email',
-    #         'first_name',
-    #         'last_name',
-    #         ]
-
-# vim: set tw=80 cc=80:
+    email = models.EmailField(verbose_name='email address',
+                              max_length=255,
+                              unique=True,
+                              blank=False)
+    email_active = models.BooleanField(verbose_name='Email Activated',
+                                       default=False)
+    object = UserManager()
--- insalan/user/views.py
+++ insalan/user/views.py
@@ -1,25 +1,36 @@
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import Group, Permission
-
-from rest_framework import viewsets
-from rest_framework import permissions
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.generics import CreateAPIView
+from rest_framework import permissions, status, viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from insalan.user.models import User
-from insalan.user.serializers import UserSerializer, \
-    GroupSerializer, \
-    PermissionSerializer
+from insalan.user.serializers import (
+    GroupSerializer,
+    PermissionSerializer,
+    UserLoginSerializer,
+    UserRegisterSerializer,
+    UserSerializer,
+)
 
 # TODO: change permission
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+
+    queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAdminUser]
+
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [SessionAuthentication]
+    serializer_class = UserSerializer
 
 
 # TODO: change permission
@@ -27,7 +38,8 @@ class PermissionViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows permissions to be viewed or edited.
     """
-    queryset = Permission.objects.all().order_by('name')
+
+    queryset = Permission.objects.all().order_by("name")
     serializer_class = PermissionSerializer
     permission_classes = [permissions.IsAdminUser]
 
@@ -37,19 +49,30 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
-    queryset = Group.objects.all().order_by('name')
+
+    queryset = Group.objects.all().order_by("name")
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAdminUser]
 
 
 # class UserRegister(APIView):
-class UserRegister(CreateAPIView):
+class UserRegister(APIView):
     """
     API endpoint that allows user creation.
-
-    Don't forget the final `/` !
     """
-    serializer_class = UserSerializer
+
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserRegisterSerializer
+
+    def post(self, request):
+        data = request.data  # TODO: add validation
+        serializer = UserRegisterSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.create(data)
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
     # def post(self, request, format=None):
     #     # TODO: Use another serializer ?
     #     # TODO: Check data validity
@@ -60,3 +83,29 @@ class UserRegister(CreateAPIView):
     #     # u: User = User.objects.create_user()
     #
     #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class UserLogin(APIView):
+    """
+    API endpoint that allows user login
+    """
+
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = [SessionAuthentication]
+
+    def post(self, request):
+        data = request.data
+        serializer = UserLoginSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.check_validity(data)
+            login(request, user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserLogout(APIView):
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = []
+
+    def post(self, request):
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
