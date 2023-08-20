ef0e71edb8386f2414f3fe3752809e3a47001540
Amélie "Lymkwi" González <lymkwi@vulpinecitrus.info>
2023-08-20 04:44:13 +0200

    ✅ Fix partner unit tests
    Fix partner unit tests by de-hardcoding IDs, and taking into account all
    of the side effects that come from running tests one after another (ie.
    IDs increase). Also authenticate queries where needed.
    
    Signed-off-by: Amélie "Lymkwi" González <lymkwi@vulpinecitrus.info>

--- insalan/partner/tests.py
+++ insalan/partner/tests.py
@@ -6,6 +6,8 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from insalan.user.models import User
+
 from .models import Partner
 
 
@@ -19,7 +21,6 @@ def create_test_img(file_name: str = 'test.png') -> SimpleUploadedFile:
     test_img.name = file_name
     return SimpleUploadedFile(test_img.name, test_img.getvalue())
 
-
 class UserTestCase(TestCase):
 
     def setUp(self) -> None:
@@ -27,7 +28,9 @@ class UserTestCase(TestCase):
                        Partner.PartnerType.PARTNER)
 
     def test_get_existing_partner(self) -> None:
-        partner = Partner.objects.get(id=1)
+        prtn = create_partner('Partner', 'http://partner.com',
+                       Partner.PartnerType.PARTNER)
+        partner = Partner.objects.get(id=prtn.id)
         self.assertEqual(partner.name, 'Partner')
         self.assertEqual(partner.url, 'http://partner.com')
         self.assertEqual(partner.partner_type, 'PA')
@@ -40,9 +43,9 @@ class UserTestCase(TestCase):
 class PartnerListViewsTest(APITestCase):
 
     def test_get(self) -> None:
-        create_partner('Partner 1', 'https://partner1.com',
+        prtn_one = create_partner('Partner 1', 'https://partner1.com',
                                  Partner.PartnerType.PARTNER)
-        create_partner('Partner 2', 'https://partner2.com',
+        prtn_two = create_partner('Partner 2', 'https://partner2.com',
                                  Partner.PartnerType.SPONSOR)
 
         response = self.client.get(reverse('partners:list'))
@@ -54,14 +57,14 @@ class PartnerListViewsTest(APITestCase):
             'previous': None,
             'results': [
                 {
-                    'id': 1,
+                    'id': prtn_one.id,
                     'name': 'Partner 1',
                     'url': 'https://partner1.com',
                     'logo': None,
                     'partner_type': 'PA',
                 },
                 {
-                    'id': 2,
+                    'id': prtn_two.id,
                     'name': 'Partner 2',
                     'url': 'https://partner2.com',
                     'logo': None,
@@ -71,6 +74,9 @@ class PartnerListViewsTest(APITestCase):
         })
 
     def test_post(self) -> None:
+        User.objects.create(username="admin", is_superuser=True, is_staff=True)
+        self.client.force_authenticate(user=User.objects.get(username="admin"))
+
         response = self.client.post(reverse('partners:list'), data={
             'name': 'New Partner',
             'url': 'https://new-partner.com',
@@ -79,28 +85,28 @@ class PartnerListViewsTest(APITestCase):
         }, format='multipart')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+        self.client.force_authenticate(user=None)
 
 class PartnerDetailViewTest(APITestCase):
-
     def test_get(self) -> None:
-        create_partner('Partner 1', 'http://partner1.com',
+        prtn = create_partner('Partner 1', 'http://partner1.com',
                        Partner.PartnerType.PARTNER)
-        response = self.client.get(reverse('partners:detail', args=[1]))
+        response = self.client.get(reverse('partners:detail', args=[prtn.id]))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {
-            'id': 1,
+            'id': prtn.id,
             'name': 'Partner 1',
             'url': 'http://partner1.com',
             'logo': None,
             'partner_type': 'PA',
         })
 
-        create_partner('Partner 2', 'http://partner2.com',
+        prtn = create_partner('Partner 2', 'http://partner2.com',
                        Partner.PartnerType.SPONSOR)
-        response = self.client.get(reverse('partners:detail', args=[2]))
+        response = self.client.get(reverse('partners:detail', args=[prtn.id]))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {
-            'id': 2,
+            'id': prtn.id,
             'name': 'Partner 2',
             'url': 'http://partner2.com',
             'logo': None,
@@ -108,42 +114,69 @@ class PartnerDetailViewTest(APITestCase):
         })
 
     def test_get_404(self) -> None:
-        response = self.client.get(reverse('partners:detail', args=[0]))
+        # Find an ID that probably nobody has
+        ids = Partner.objects.all().values_list(flat=True)
+        max_id = max(ids) + 10 if len(ids) > 0 else 10
+        response = self.client.get(reverse('partners:detail', args=[max_id]))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_put(self) -> None:
-        create_partner('partner 1', 'http://partner1.com',
+        prtn = create_partner('partner 1', 'http://partner1.com',
                        Partner.PartnerType.PARTNER)
-        response = self.client.put(reverse('partners:detail', args=[1]), data={
+
+        User.objects.create(username="admin", is_superuser=True, is_staff=True)
+        self.client.force_authenticate(user=User.objects.get(username="admin"))
+
+        response = self.client.put(reverse('partners:detail', args=[prtn.id]), data={
             'name': 'updated Partner 1',
             'url': 'http://updated-partner1.com',
             'logo': create_test_img('test-put.png'),
             'partner_type': Partner.PartnerType.SPONSOR,
         }, format='multipart')
+        
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+        self.client.force_authenticate(user=None)
+
     def test_patch(self) -> None:
-        create_partner('Partner 1', 'http://partner1.com',
+        prtn = create_partner('Partner 1', 'http://partner1.com',
                        Partner.PartnerType.PARTNER)
-        response = self.client.patch(reverse('partners:detail', args=[1]),
+
+        User.objects.create(username="admin", is_superuser=True, is_staff=True)
+        self.client.force_authenticate(user=User.objects.get(username="admin"))
+
+        response = self.client.patch(reverse('partners:detail', args=[prtn.id]),
                                      data={'name': 'updated Partner 1'},
                                      format='multipart')
+        
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
         self.assertEqual(response.data, {
-            'id': 1,
+            'id': prtn.id,
             'name': 'updated Partner 1',
             'url': 'http://partner1.com',
             'logo': None,
             'partner_type': 'PA',
         })
 
+        self.client.force_authenticate(user=None)
+
     def test_delete(self) -> None:
-        create_partner('Partner 1', 'http://partner1.com',
+        prtn = create_partner('Partner 1', 'http://partner1.com',
                        Partner.PartnerType.PARTNER)
-        response = self.client.get(reverse('partners:detail', args=[1]))
+        prtn_id = prtn.id
+
+        response = self.client.get(reverse('partners:detail', args=[prtn_id]))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        self.client.delete(reverse('partners:detail', args=[1]))
+        User.objects.create(username="admin", is_superuser=True, is_staff=True)
+        self.client.force_authenticate(user=User.objects.get(username="admin"))
 
-        response = self.client.get(reverse('partners:detail', args=[1]))
+        response = self.client.delete(reverse('partners:detail', args=[prtn_id]))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        self.client.force_authenticate(user=None)
+
+        response = self.client.get(reverse('partners:detail', args=[prtn_id]))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
