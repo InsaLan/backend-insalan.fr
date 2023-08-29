f07b6df629312eede266607ba78da1af1531259f
Aurore Poirier <Lugrim@users.noreply.github.com>
2023-08-30 00:13:41 +0200

    🚨 Fixed user serializers lints

--- insalan/user/serializers.py
+++ insalan/user/serializers.py
@@ -1,23 +1,30 @@
+"""Data Serializers for the InsaLan User module"""
+
 from django.contrib.auth.models import Group, Permission
 from django.contrib.auth import authenticate
-from rest_framework import serializers, status
-from rest_framework.response import Response
-from django.utils.translation import gettext_lazy as _
-from .models import User
-from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import User
+
+# pylint: disable=too-few-public-methods
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """Serializer for an User"""
     class Meta:
+        """Meta class, used to set parameters"""
         model = User
         exclude = ("password",)
         # fields = ['url', 'username', 'email', 'groups']
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
+    """Serializer for a register for submission"""
     email = serializers.EmailField(
-        required=True, validators=[UniqueValidator(queryset=User.objects.all())]
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())],
     )
     password = serializers.CharField(
         write_only=True, required=True, validators=[validate_password]
@@ -27,6 +34,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     password_validation = serializers.CharField(write_only=True, required=True)
 
     class Meta:
+        """Meta class, used to set parameters"""
         model = User
         fields = [
             "username",
@@ -58,28 +66,40 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
 
 class UserLoginSerializer(serializers.Serializer):
+    """Serializer for a login form submission"""
     username = serializers.CharField()
     password = serializers.CharField()
 
     class Meta:
+        """Meta class, used to set parameters"""
         model = User
 
     def check_validity(self, data):
-        user = authenticate(username=data["username"], password=data["password"])
+        """
+        Checks thats:
+            - Username & Password combination gives a good user
+            - The account has not been deactivated
+        """
+        user = authenticate(username=data["username"],
+                            password=data["password"])
         if user is not None:
-            if not user.is_active:
+            if not user.is_active or not user.email_active:
                 raise serializers.ValidationError(_("Account not actived"))
         return user
 
 
 class PermissionSerializer(serializers.HyperlinkedModelSerializer):
+    """Serializer for a django Permission"""
     class Meta:
+        """Meta class, used to set parameters"""
         model = Permission
         exclude = ("content_type",)
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    """Serializer for a django Group (used with Permissions)"""
     class Meta:
+        """Meta class, used to set parameters"""
         model = Group
         exclude = ()
         # fields = ['url', 'name']
