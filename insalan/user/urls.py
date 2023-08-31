e23458666f90743fd29ac639d0a9c6bed9f2dadb
Mahal <teo.nespoulet@protonmail.com>
2023-08-31 15:39:18 +0200

    adding csrf endpoint

--- insalan/user/urls.py
+++ insalan/user/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("logout/", views.UserLogout.as_view(), name="/logout"),
     path("user/", views.UserView.as_view(), name="/"),
     path("me/", views.UserMe.as_view(), name="me"),
+    path("get-csrf/", views.get_csrf, name="get-csrf"),
 ]
