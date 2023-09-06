4a168815ea73f410d8cbf5f2cde7094e20b92578
Mahal <teo.nespoulet@protonmail.com>
2023-09-06 18:50:22 +0200

    adding transaction model 1/n

--- insalan/payment/models.py
+++ insalan/payment/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from insalan.user.models import User
 
-# Create your models here.
+class TransactionStatus(models.TextChoices):
+    """Information about the current transaction status"""
+
+    FAILED = "FAILED", _("Fail")
+    SUCCEDED = "SUCCEEDED", _("Success")
+
+
+class Transaction(models.Model):
+    """A transaction"""
+    payer = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(null=False)
+    payment_status = models.CharField(
+        max_length=10,
+        blank=True,
+        default=TransactionStatus.FAILED,
+        choices=TransactionStatus.choices,
+        null=False,
+        verbose_name="Transaction status",
+    )
+    date = models.DateField()
+
