3045f3139ef464da3b6e655cf099135258852bf3
Mahal <teo.nespoulet@protonmail.com>
2023-09-28 17:44:49 +0200

    removing hardcoded superuser

--- entrypoint-dev.sh
+++ entrypoint-dev.sh
@@ -5,7 +5,7 @@ python3 manage.py migrate --run-syncdb
 python3 manage.py collectstatic --noinput
 
 # Create a superuser for easier development
-DJANGO_SUPERUSER_EMAIL=admin@insalan.fr DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_PASSWORD=password \
+DJANGO_SUPERUSER_EMAIL=admin@insalan.fr DJANGO_SUPERUSER_USERNAME=${SUPERUSER_USER} DJANGO_SUPERUSER_PASSWORD=${SUPERUSER_PASS} \
 python3 manage.py createsuperuser --noinput
 
 echo "Running Django development server on port 8000"
