7323e0b42ac005a75cd14744c7d210085f85f1c5
Aurore Poirier <Lugrim@users.noreply.github.com>
2023-10-10 19:25:19 +0200

    Removed legacy `email_active` field

--- insalan/user/models.py
+++ insalan/user/models.py
@@ -94,9 +94,6 @@ class User(AbstractUser, PermissionsMixin):
     email = models.EmailField(
         verbose_name=_("Courriel"), max_length=255, unique=True, blank=False
     )
-    email_active = models.BooleanField(
-        verbose_name=_("Courriel vérifié"), default=False
-    )
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
     is_staff = models.BooleanField(
