19aa0d77726aaf5e46a11c0b1065cd7a6808056e
KwikKill <gabriel@blaisot.org>
2023-10-11 23:24:44 +0200

    Fix bad url in users endpoint

--- insalan/user/urls.py
+++ insalan/user/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("register/", views.UserRegister.as_view(), name="/register"),
     path("login/", views.UserLogin.as_view(), name="/login"),
     path("logout/", views.UserLogout.as_view(), name="/logout"),
-    path("user/", views.UserView.as_view(), name="/"),
+    path("data/<int:pk>/", views.UserView.as_view(), name="/"),
     path("me/", views.UserMe.as_view(), name="me"),
     path(
         "resend-email/", views.ResendEmailConfirmView.as_view(), name="resend-confirm"
