54e0c9868e1496cdf31015c05697233e70af213c
Mahal <teo.nespoulet@protonmail.com>
2023-10-12 14:20:01 +0200

    adding verbose_name

--- insalan/payment/apps.py
+++ insalan/payment/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class PaymentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'insalan.payment'
+    name ='insalan.payment'
+    verbose_name = _('Paiement')
