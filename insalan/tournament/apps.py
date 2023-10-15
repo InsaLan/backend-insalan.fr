0c381c9414bf49e69f75e96e28881aef46ae23bb
Amélie "Lymkwi" González <lymkwi@vulpinecitrus.info>
2023-10-15 20:55:19 +0200

    💶 Introduce the product category hook system
    This commit introduces the product category hook system, which allows
    applications to create a class derived from an interface that implements
    callbacks for payment success and failure handling.
    
    A module can simply inherit the `PaymentHooks` class in one of its
    classes, and register it with the `PaymentCallbackSystem` class.
    
    An example is provided with the tournament class, which now implements a
    way to validate a pending registration with a payment.

--- insalan/tournament/apps.py
+++ insalan/tournament/apps.py
@@ -2,10 +2,15 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
-
 class TournamentConfig(AppConfig):
     """Tournament app config"""
 
     default_auto_field = 'django.db.models.BigAutoField'
     name = "insalan.tournament"
     verbose_name = _("Tournois")
+
+    def ready(self):
+        """Called when the module is ready"""
+        from .payment import payment_handler_register
+
+        payment_handler_register()
