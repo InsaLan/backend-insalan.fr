af6023bae821bc8b72c437ecbc60e7d6edf047d7
Mahal <12588690+ShiroUsagi-san@users.noreply.github.com>
2023-10-20 00:20:44 +0200

    Adding validator on content in the cms module (#79)
    * adding a validator on content and constants
    
    * removing trailing debug artefacts
    
    * fixing typo
    
    * fixing field's name
    
    * fixing minors issues and changing kwargs argument name

--- insalan/cms/tests.py
+++ insalan/cms/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
-# Create your tests here.
+from insalan.cms.models import ( 
+    Constant,
+    Content
+)
+
+
+class ContentTestCase(TestCase):
+    def test_content_with_constants(self):
+        """ test that we can create a content using constant """
+        Constant.objects.create(name="a", value="je suis la valeur a")
+        Constant.objects.create(name="b", value="je suis la valeur b")
+        Content.objects.create(name="content_a", content="j'appelle la constante ${a} et la constante ${b}")
+
+    def test_content_with_unknow_constants(self):
+        Constant.objects.create(name="a", value="je suis la valeur a")
+        Content.objects.create(name="content", content="je suis ${a} mais aussi une variable ${random}")
+        self.assertRaises(ValidationError)
+
+    def test_display_undefined_constant_list(self):
+        Content.objects.create(name="content", content="je test ${inconnue} mais aussi une variable ${random}")
+        self.assertRaisesMessage(ValidationError, _("Des constantes non définies sont utilisées: inconnue, random"))
