edc7189379d89c794925207519237d9b128f71cd
Mahal <12588690+ShiroUsagi-san@users.noreply.github.com>
2023-10-20 12:53:33 +0200

    adding is_announced in the list display

--- insalan/tournament/admin.py
+++ insalan/tournament/admin.py
@@ -33,7 +33,7 @@ admin.site.register(Game, GameAdmin)
 class TournamentAdmin(admin.ModelAdmin):
     """Admin handler for Tournaments"""
 
-    list_display = ("id", "name", "event", "game")
+    list_display = ("id", "name", "event", "game", "is_announced")
     search_fields = ["name", "event", "game"]
 
 
