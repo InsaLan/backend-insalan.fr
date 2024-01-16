6a861f4ddb4fc8882cd4e5c0677dcd7b7dc08cc1
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2024-01-16 13:13:52 +0100

    split tournament models into seperate files

---
+++ insalan/tournament/models/event.py
@@ -0,0 +1,75 @@
+from typing import List
+from django.db import models
+from django.core.validators import (
+    FileExtensionValidator,
+    MaxValueValidator,
+    MinValueValidator,
+    MinLengthValidator,
+)
+from django.utils.translation import gettext_lazy as _
+
+from . import tournament
+
+class Event(models.Model):
+    """
+    An Event is any single event that is characterized by the following:
+     - A single player can only register for a single tournament per event
+
+    This means, for example, that if we want to let players play in the smash
+    tournaments and InsaLan one year, we can have two events happening
+    concurrently.
+    """
+
+    name = models.CharField(
+        verbose_name=_("Nom de l'évènement"),
+        max_length=40,
+        validators=[MinLengthValidator(4)],
+        null=False,
+    )
+    description = models.CharField(
+        verbose_name=_("Description de l'évènement"),
+        max_length=128,
+        default="",
+        blank=True,
+    )
+    year = models.IntegerField(
+        verbose_name=_("Année"), null=False, validators=[MinValueValidator(2003)]
+    )
+    month = models.IntegerField(
+        verbose_name=_("Mois"),
+        null=False,
+        validators=[MinValueValidator(1), MaxValueValidator(12)],
+    )
+    ongoing = models.BooleanField(verbose_name=_("En cours"), default=False)
+    logo: models.FileField = models.FileField(
+        verbose_name=_("Logo"),
+        blank=True,
+        null=True,
+        upload_to="event-icons",
+        validators=[
+            FileExtensionValidator(allowed_extensions=["png", "jpg", "jpeg", "svg", "webp"])
+        ],
+    )
+
+    class Meta:
+        """Meta options"""
+
+        verbose_name = _("Évènement")
+        verbose_name_plural = _("Évènements")
+
+    def __str__(self) -> str:
+        """Format this Event to a str"""
+        return f"{self.name} ({self.year}-{self.month:02d})"
+
+    def get_tournaments_id(self) -> List[int]:
+        """Return the list of tournaments identifiers for that Event"""
+        return tournament.Tournament.objects.filter(event=self).values_list("id", flat=True)
+
+    def get_tournaments(self) -> List["Tournament"]:
+        """Return the list of tournaments for that Event"""
+        return tournament.Tournament.objects.filter(event=self)
+
+    @staticmethod
+    def get_ongoing_ids() -> List[int]:
+        """Return the identifiers of ongoing events"""
+        return __class__.objects.filter(ongoing=True).values_list("id", flat=True)
---
+++ insalan/tournament/models/manager.py
@@ -0,0 +1,79 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+from insalan.tickets.models import Ticket
+from insalan.user.models import User
+
+from .payement_status import PaymentStatus
+
+class Manager(models.Model):
+    """
+    A Manager is someone in charge of heading a team of players.
+    """
+
+    user = models.ForeignKey(
+        User,
+        verbose_name=_("Utilisateur⋅ice"),
+        on_delete=models.CASCADE,
+    )
+    team = models.ForeignKey(
+        "tournament.Team",
+        verbose_name=_("Équipe"),
+        on_delete=models.CASCADE
+    )
+    payment_status = models.CharField(
+        verbose_name=_("Statut du paiement"),
+        max_length=10,
+        blank=True,
+        default=PaymentStatus.NOT_PAID,
+        choices=PaymentStatus.choices,
+        null=False,
+    )
+    ticket = models.ForeignKey(
+        Ticket,
+        on_delete=models.SET_NULL,
+        verbose_name=_("Ticket"),
+        null=True,
+        blank=True,
+        default=None,
+    )
+
+    class Meta:
+        """Meta Options"""
+
+        verbose_name = _("Inscription d'un⋅e manager")
+        verbose_name_plural = _("Inscriptions de managers")
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "team"], name="not_twice_same_manager"
+            )
+        ]
+
+    def __str__(self) -> str:
+        """Format this manager registration as a str"""
+        return f"(Manager) {self.user.username} for {self.team}"
+
+    def as_user(self) -> User:
+        """Return the current player as a User object"""
+        return self.user
+
+    def get_team(self):
+        """Return the Team object of the current team"""
+        return self.team
+
+    def clean(self):
+        """
+        Assert that the user associated with the provided manager does not already
+        exist in any team of any tournament of the event
+        """
+        user = self.user
+        event = self.get_team().get_tournament().get_event()
+        if not unique_event_registration_validator(user,event, manager=self.id):
+            raise ValidationError(
+                _("Utilisateur⋅rice déjà inscrit⋅e dans un tournoi de cet évènement")
+            )
+        if not tournament_announced(self.team.get_tournament()):
+            raise ValidationError(
+                _("Tournoi non annoncé")
+            )
