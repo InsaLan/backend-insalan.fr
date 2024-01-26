9707de3c98be1ee6aa88f92ff89109adf4201183
KwikKill <gabriel@blaisot.org>
2024-01-26 08:55:31 +0100

    Add pizza endpoints

--- insalan/pizza/serializers.py
+++ insalan/pizza/serializers.py
@@ -1,7 +1,8 @@
 """ Serializers for pizza models"""
 from rest_framework import serializers
 
-from .models import Pizza, Order
+from .models import Pizza, Order, TimeSlot, PizzaOrder
+from typing import List
 
 class PizzaSerializer(serializers.ModelSerializer):
     """Serializer for a pizza model"""
@@ -9,10 +10,54 @@ class PizzaSerializer(serializers.ModelSerializer):
         model = Pizza
         fields = "__all__"
 
+class PizzaIdSerializer(serializers.Serializer):
+    """Serializer to verify a list of pizza IDs"""
+
+    def to_representation(self, instance):
+        """Turn a Django object into a serialized representation"""
+        return instance.id
 
 class OrderSerializer(serializers.ModelSerializer):
     """ Serializer for an order"""
     pizza = List
+    user = serializers.CharField(required=False, source="get_username")
+
     class Meta:
         model = Order
         read_only_fields = ("id", )
+        fields = ("id", "user", "time_slot", "pizza", "price", "paid", "created_at", "delivered", "delivery_date")
+
+class OrderIdSerializer(serializers.ModelSerializer):
+    """ Serializer for an order"""
+    
+    def to_representation(self, instance):
+        """Turn a Django object into a serialized representation"""
+        return instance.id
+
+class TimeSlotSerializer(serializers.ModelSerializer):
+    """Serializer for a timeslot model"""
+    orders = serializers.ListField(required=False, source="get_orders_id")
+
+    class Meta:
+        model = TimeSlot
+        fields = "__all__"
+
+class TimeSlotIdSerializer(serializers.ModelSerializer):
+    """Serializer for a timeslot model"""
+    def to_representation(self, instance):
+        """Turn a Django object into a serialized representation"""
+        return instance.id
+
+class PizzaByOrderSerializer(serializers.ModelSerializer):
+    pizza = serializers.SerializerMethodField()
+
+    class Meta:
+        model = TimeSlot
+        fields = ("id", "pizza", "delivery_time", "start", "end", "pizza_max", "public", "ended")
+
+    def get_pizza(self, obj):
+        result = {}
+        # for each pizza type in the timeslot, count the number of pizza ordered
+        for pizza in PizzaOrder.objects.filter(order__time_slot__id=obj.id).values("pizza__id").distinct():
+            result[pizza["pizza__id"]] = PizzaOrder.objects.filter(order__time_slot__id=obj.id, pizza__id=pizza["pizza__id"]).count()
+        return result
--- insalan/pizza/views.py
+++ insalan/pizza/views.py
@@ -5,29 +5,48 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 
 from .models import Pizza, TimeSlot, PizzaOrder, Order
-from .serializers import PizzaSerializer
+from .serializers import PizzaSerializer, PizzaIdSerializer, OrderSerializer, TimeSlotSerializer, PizzaByOrderSerializer, TimeSlotIdSerializer, OrderIdSerializer
+from rest_framework.pagination import PageNumberPagination
 
 class ReadOnly(permissions.BasePermission):
     def has_permissions(self, request, view) -> bool:
         return request.method in permissions.SAFE_METHODS
 
+"""
+WORK IN PROGRESS: 
+- missing POST, PUT, DELETE for endpoints
+- missing permissions
+- missing tests
+"""
 
-class PizzaListFull(generics.ListCreateAPIView):
+class PizzaPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+class PizzaList(generics.ListAPIView):
+    """List all pizzas id"""
+    pagination_class = None
+    queryset = Pizza.objects.all().order_by("name")
+    serializer_class = PizzaIdSerializer
+    permissions_classes = [ReadOnly]
+
+class PizzaListFull(generics.ListAPIView):
+    """List all pizzas"""
     pagination_class = None
     queryset = Pizza.objects.all().order_by("name")
     serializer_class = PizzaSerializer
-    permissions_classes = [permissions.IsAdminUser | ReadOnly]
+    permissions_classes = [ReadOnly]
 
-class PizzaDetail(generics.ListAPIView):
+class PizzaDetail(generics.RetrieveAPIView):
     """Find a pizza by its id"""
-    #TODO: return 404 when no pizza found
     pagination_class = None
+    queryset = Pizza.objects.all().order_by("name")
     serializer_class = PizzaSerializer
-
-    def get_queryset(self):
-         return Pizza.objects.filter(id=self.kwargs["id"]) 
+    permissions_classes = [ReadOnly]
 
 class PizzaSearch(generics.ListAPIView):
+    """Search a pizza by its name"""
     pagination_class = None
     serializer_class = PizzaSerializer
 
@@ -35,17 +54,118 @@ class PizzaSearch(generics.ListAPIView):
         partial_name = self.request.query_params.get("q", None)
         return Pizza.objects.filter(name__contains=partial_name)
 
-class OrderDetail(generics.ListAPIView):
+
+class PizzaListByGivenOrder(generics.RetrieveAPIView):
+    """Group pizzas by order"""
     pagination_class = None
     serializer_class = OrderSerializer
-
-    permissions_classes = [ permissions.IsAdminUser ]
-
-    def get_queryset(self):
-        queryset = Order.objects.filter(id=self.kwargs["id"])
-
-class PizzaListByOrder(generics.ListAPiView):
-    pass
+    queryset = Order.objects.all()
 
 class PizzaListByTimeSlot(generics.ListAPIView):
-    pass
+    """Group pizzas by timeslot"""
+    pagination_class = PizzaPagination
+    serializer_class = PizzaByOrderSerializer
+    queryset = TimeSlot.objects.all()
+
+class PizzaListByGivenTimeSlot(generics.RetrieveAPIView):
+    """Group pizzas by timeslot"""
+    pagination_class = None
+
+    def get(self, request, *args, **kwargs):
+        timeslot = TimeSlot.objects.get(id=self.kwargs["pk"])
+        serializer = PizzaByOrderSerializer(timeslot)
+        # only return the pizzas ordered
+        return Response(serializer.data)
+
+class TimeSlotList(generics.ListAPIView):
+    """List all timeslots"""
+    pagination_class = PizzaPagination
+    queryset = TimeSlot.objects.all()
+    serializer_class = TimeSlotIdSerializer
+    permissions_classes = [ReadOnly]
+
+
+class TimeSlotListFull(generics.ListAPIView):
+    """List all timeslots"""
+    pagination_class = PizzaPagination
+    queryset = TimeSlot.objects.all()
+    serializer_class = TimeSlotSerializer
+    permissions_classes = [ReadOnly]
+
+
+class TimeSlotDetail(generics.RetrieveAPIView):
+    """Find a timeslot by its id"""
+    queryset = TimeSlot.objects.all()
+    permissions_classes = [ReadOnly]
+
+    def get(self, request, *args, **kwargs):
+        timeslot = TimeSlot.objects.get(id=self.kwargs["pk"])
+        serializer = TimeSlotSerializer(timeslot, context={"request": request}).data
+
+        order_serialized = []
+        for order in serializer["orders"]:
+            order = Order.objects.get(id=order)
+            order_serializer = OrderSerializer(order).data
+            order_serializer.pop("time_slot")
+            order_serialized.append(order_serializer)
+        serializer["orders"].clear()
+        serializer["orders"] = order_serialized
+
+        serializer.pop("player_product")
+        serializer.pop("staff_product")
+        serializer.pop("external_product")
+
+        return Response(serializer)
+
+class NextTimeSlot(generics.ListAPIView):
+    """Find the next timeslot"""
+    queryset = TimeSlot.objects.all()
+    permissions_classes = [ReadOnly]
+
+    def get(self, request, *args, **kwargs):
+        # select incoming timeslot and currents timeslot
+        timeslot = TimeSlot.objects.filter(ended=False).order_by("start")
+
+        serializers = []
+        for ts in timeslot:
+            serializer = TimeSlotSerializer(ts, context={"request": request}).data
+
+            serializer.pop("player_product")
+            serializer.pop("staff_product")
+            serializer.pop("external_product")
+
+            serializers.append(serializer)
+
+        return Response(serializers)
+    
+class OrderList(generics.ListAPIView):
+    """List all orders"""
+    queryset = Order.objects.all()
+    serializer_class = OrderIdSerializer
+    permissions_classes = [ReadOnly]
+
+class OrderListFull(generics.ListAPIView):
+    """List all orders"""
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permissions_classes = [ReadOnly]
+
+class OrderDetail(generics.RetrieveAPIView):
+    """Find an order by its id"""
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permissions_classes = [ReadOnly]
+    queryset = Order.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        order = Order.objects.get(id=self.kwargs["pk"])
+        serializer = OrderSerializer(order, context={"request": request}).data
+
+        timeslot = TimeSlot.objects.get(id=serializer["time_slot"])
+        timeslot_serializer = TimeSlotSerializer(timeslot).data
+        timeslot_serializer.pop("player_product")
+        timeslot_serializer.pop("staff_product")
+        timeslot_serializer.pop("external_product")
+        serializer["time_slot"] = timeslot_serializer
+
+        return Response(serializer)
