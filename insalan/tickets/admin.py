b3862aa501eff55021f73585369eb194db27e496
KwikKill <gabriel@blaisot.org>
2024-01-31 14:07:40 +0100

    Add admin action to manually send ticket

--- insalan/tickets/admin.py
+++ insalan/tickets/admin.py
@@ -1,9 +1,12 @@
 """
 This module contains the admin configuration for the Ticket model in the Insalan application.
 """
-
+from django.utils.translation import gettext as _
 from django.contrib import admin
+from django.contrib import messages
 
+from insalan.settings import EMAIL_AUTH
+from insalan.mailer import MailManager
 from .models import Ticket
 
 
@@ -13,6 +16,15 @@ class TicketAdmin(admin.ModelAdmin):
     """
     list_display = ("id", "user", "status", "tournament", "token")
     search_fields = ["user__username", "user__email", "tournament__name", "token"]
+    actions = ["send_tickets_to_mail"]
 
+    @admin.action(description=_("Envoyer les tickets par mail"))
+    def send_tickets_to_mail(self, request, queryset):
+        """
+        Action to send tickets to mail
+        """
+        for ticket in queryset:
+            MailManager.get_mailer(EMAIL_AUTH["contact"][0]).send_ticket_mail(ticket.user, ticket)
+        messages.info(request, _("Les tickets sélectionnés sont en cours d'envoi."))
 
 admin.site.register(Ticket, TicketAdmin)
