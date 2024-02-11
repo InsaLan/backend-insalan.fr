4542e8aba32cbd3c1e48aa1e3b6c90a0bd09479f
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2024-02-11 18:08:45 +0100

    adapt bracket matchs creation for non powers of 2

--- insalan/tournament/manage/bracket.py
+++ insalan/tournament/manage/bracket.py
@@ -1,4 +1,5 @@
 from ..models import Bracket, KnockoutMatch, BracketType, BracketSet
+from math import ceil
 
 def create_empty_knockout_matchs(bracket: Bracket):
     depth = bracket.get_depth()
@@ -6,13 +7,15 @@ def create_empty_knockout_matchs(bracket: Bracket):
     for match in KnockoutMatch.objects.filter(bracket=bracket):
         match.delete()
 
-    for round_n in range(1,depth+1):
-        for match_id in range(1,2**(round_n-1)+1):
-            KnockoutMatch.objects.create(round_number=round_n,index_in_round=match_id,bracket=bracket)
+    for roundIdx in range(1,depth+1):
+        matchCount = min(2**(roundIdx-1),ceil(bracket.get_max_match_count()/2**(depth-roundIdx)))
+        for matchId in range(1,matchCount+1):
+            KnockoutMatch.objects.create(round_number=roundIdx,index_in_round=matchId,bracket=bracket)
     if bracket.bracket_type == BracketType.DOUBLE:
-        for round_n in range(1,2*depth-1):
-            for match_id in range(1,2**((round_n-1)//2)+1):
-                KnockoutMatch.objects.create(round_number=round_n,index_in_round=match_id,bracket=bracket,bracket_set=BracketSet.LOOSER)
+        for roundIdx in range(1,2*depth-1):
+            matchCount = min(2**((roundIdx-1)//2),ceil((bracket.get_max_match_count()//2)/2**(depth-(roundIdx+1)//2 - 1)))
+            for matchId in range(1,matchCount+1):
+                KnockoutMatch.objects.create(round_number=roundIdx,index_in_round=matchId,bracket=bracket,bracket_set=BracketSet.LOOSER)
         KnockoutMatch.objects.create(round_number=0,index_in_round=1,bracket=bracket)
 
 # def fill_knockout_matchs(bracket: Bracket):
