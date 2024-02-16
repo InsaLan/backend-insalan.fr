8635394d0be199d5f2caa87bed2e8b4376338bb5
Lugrim <Lugrim@users.noreply.github.com>
2024-02-16 13:40:08 +0100

    ⚙ Added helper fields for langate staff autologin

--- insalan/langate/serializers.py
+++ insalan/langate/serializers.py
@@ -13,6 +13,8 @@ class SimplifiedUserDataSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=100)
     name = serializers.CharField(max_length=100)
     email = serializers.EmailField(max_length=255)
+    is_staff = serializers.BooleanField()
+    is_admin = serializers.BooleanField()
 
 
 class TournamentRegistrationSerializer(serializers.Serializer):
