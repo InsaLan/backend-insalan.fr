1569b9bd1f6494e180144ef74fd574e534ecf51e
KwikKill <gabriel@blaisot.org>
2024-03-07 23:48:42 +0100

    Add tournament ordering

---
+++ insalan/admin.py
@@ -0,0 +1,26 @@
+# Covering django.contrib.admin.AdminSite.get_app_list
+from django.contrib import admin
+
+ADMIN_ORDERING = []
+
+# Creating a sort function
+def get_app_list(self, request):
+    app_dict = self._build_app_dict(request)
+
+    ordering = []
+    # For each app in the app list
+    for app_name, app_data in app_dict.items():
+        # If app_name is not in ADMIN_ORDERING
+        if app_name not in [x[0] for x in ADMIN_ORDERING]:
+            # Add basic ordering
+            ordering.append((app_name, [x['object_name'] for x in app_data['models']]))
+    ordering += ADMIN_ORDERING
+
+    # For each app in the Ordering list
+    for app_name, object_list in ordering:
+        app = app_dict[app_name]
+        # Sort the models
+        app['models'].sort(key=lambda x: object_list.index(x['object_name']))
+        yield app
+
+admin.AdminSite.get_app_list = get_app_list
