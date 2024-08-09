59a9a1e595aa4325367fd448818f332d11a43f23
Ala Al-Deen Alkhafaji <3akevdev@gmail.com>
2024-08-09 22:43:50 +0200

    refactor JS code, accept parameters from django

--- assets/js/event_admin.js
+++ assets/js/event_admin.js
@@ -1,13 +1,13 @@
-const gridSize = 25; // size of each grid cell
-const pickedColor = "blue";
-const notPickedColor = "white";
+let cellSize = 25;
+let pickedColor = "blue";
+let notPickedColor = "white";
 let currentSeats = [];
 
 function drawGrid() {
-  const canvas = document.getElementById("seatCanvas");
+  const canvas = document.getElementById("seat_canvas");
   const ctx = canvas.getContext("2d");
 
-  const gridCount = canvas.width / gridSize; // number of grid cells
+  const gridCount = canvas.width / cellSize; // number of grid cells
 
   ctx.strokeStyle = 'black'; // color of the grid lines
 
@@ -15,27 +15,31 @@ function drawGrid() {
   for (let i = 0; i <= gridCount; i++) {
     // vertical lines
     ctx.beginPath();
-    ctx.moveTo(i * gridSize, 0);
-    ctx.lineTo(i * gridSize, canvas.height);
+    ctx.moveTo(i * cellSize, 0);
+    ctx.lineTo(i * cellSize, canvas.height);
     ctx.stroke();
 
     // horizontal lines
     ctx.beginPath();
-    ctx.moveTo(0, i * gridSize);
-    ctx.lineTo(canvas.width, i * gridSize);
+    ctx.moveTo(0, i * cellSize);
+    ctx.lineTo(canvas.width, i * cellSize);
     ctx.stroke();
   }
 }
 
-function placePixel(ctx, gridX, gridY, color) {
-  ctx.fillStyle = color;
-  // offsets to keep grid lines
-  ctx.fillRect(gridX * gridSize + 1, gridY * gridSize + 1, gridSize - 2, gridSize - 2);
+function redrawCanvas() {
+  const canvas = document.getElementById("seat_canvas");
+  const ctx = canvas.getContext("2d");
 
-  if (color === pickedColor) {
-    currentSeats.push([gridX, gridY]);
-  } else if (color === notPickedColor) {
-    currentSeats = currentSeats.filter(seat => seat[0] !== gridX || seat[1] !== gridY);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawGrid();
+
+  ctx.fillStyle = pickedColor;
+  for (let seat of currentSeats) {
+    let x = seat[0];
+    let y = seat[1];
+    ctx.fillRect(x * cellSize + 1, y * cellSize + 1, cellSize - 2, cellSize - 2);
   }
 }
 
@@ -43,66 +47,51 @@ window.onload = function () {
 
   // set up form submission hook
   var form = document.getElementById("event_form");
-  var canvasInput = document.createElement("input");
-  canvasInput.setAttribute("name", "seats");
-  canvasInput.setAttribute("type", "hidden");
-  canvasInput.setAttribute("value", "");
-  form.appendChild(canvasInput);
+  var seats_input = document.getElementById("id_seats");
 
   form.addEventListener("submit", event => {
     pixels = JSON.stringify(currentSeats);
-    canvasInput.setAttribute("value", pixels);
+    seats_input.setAttribute("value", pixels);
   });
 
-  var canvas = document.getElementById("seatCanvas");
+  var canvas = document.getElementById("seat_canvas");
   var ctx = canvas.getContext("2d");
 
-  // get old seats
-  let oldSeats = document.getElementById("id_oldseats").value;
-  JSON.parse(oldSeats).forEach(seat => {
-    currentSeats.push(seat)
-    placePixel(ctx, seat[0], seat[1], pickedColor);
+  // get params from the hidden input
+  let params = JSON.parse(document.getElementById("id_canvas_params").value)
+
+  cellSize = params.cellSize;
+  pickedColor = params.pickedColor;
+  notPickedColor = params.notPickedColor;
+
+  // draw old seats
+  params.oldSeats.forEach(seat => {
+    currentSeats.push(seat);
   });
 
-  let data = {
-    lastHoveredCell: null,
-    oldState: null,
-  }
+  redrawCanvas();
 
-  drawGrid();
-
+  let lastHoveredCell = null;
   canvas.addEventListener('mousemove', event => {
     const bounding = canvas.getBoundingClientRect();
     const x = event.clientX - bounding.left;
     const y = event.clientY - bounding.top;
 
     // calculate the grid cell coordinates
-    const gridX = Math.floor(x / gridSize);
-    const gridY = Math.floor(y / gridSize);
+    const gridX = Math.floor(x / cellSize);
+    const gridY = Math.floor(y / cellSize);
 
-    let lastHoveredCell = data.lastHoveredCell;
-
-    // if the mouse has moved to a new cell, redraw the grid
+    // if the mouse has moved to a new cell, redraw
     if (lastHoveredCell === null || gridX !== lastHoveredCell.x || gridY !== lastHoveredCell.y) {
-      // restore the canvas to its state before the outline was drawn
-      if (data.oldState !== null) {
-        ctx.putImageData(data.oldState, 0, 0);
-      }
-
-      // save the current state of the canvas
-      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      redrawCanvas();
 
       // draw the outline on the current cell
       ctx.strokeStyle = 'red'; // color of the outline
       ctx.lineWidth = 2; // width of the outline
-      ctx.strokeRect(gridX * gridSize, gridY * gridSize, gridSize, gridSize);
+      ctx.strokeRect(gridX * cellSize, gridY * cellSize, cellSize, cellSize);
 
-      // store the current cell and the image data
-      data.lastHoveredCell = { x: gridX, y: gridY };
-      data.oldState = imageData
     }
   });
-
 
   canvas.addEventListener('click', event => {
     const bounding = canvas.getBoundingClientRect();
@@ -110,19 +99,15 @@ window.onload = function () {
     const y = event.clientY - bounding.top;
 
     // calculate the grid cell coordinates
-    const gridX = Math.floor(x / gridSize);
-    const gridY = Math.floor(y / gridSize);
+    const gridX = Math.floor(x / cellSize);
+    const gridY = Math.floor(y / cellSize);
 
-    ctx.putImageData(data.oldState, 0, 0);
+    if (currentSeats.some(seat => seat[0] === gridX && seat[1] === gridY)) {
+      currentSeats = currentSeats.filter(seat => seat[0] !== gridX || seat[1] !== gridY);
+    } else {
+      currentSeats.push([gridX, gridY]);
+    }
 
-    // javascript is cursed
-    let picked = currentSeats.some(seat => seat[0] === gridX && seat[1] === gridY);
-    let color = picked ? notPickedColor : pickedColor;
-
-    placePixel(ctx, gridX, gridY, color);
-
-    // save the current state of the canvas
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    data.oldState = imageData
+    redrawCanvas();
   });
 }
