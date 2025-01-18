b51e9a8e8e3982f836a7fa446b77ad2d0a79ed50
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2025-01-18 21:16:29 +0100

    add team seeding and endpoint to change multiple ones

--- insalan/tournament/admin.py
+++ insalan/tournament/admin.py
@@ -465,7 +465,7 @@ class TeamCreationForm(forms.ModelForm):
         Meta options for the form
         """
         model = Team
-        fields = ("name", "tournament", "validated")
+        fields = ("name", "tournament", "validated", "seed")
         field_classes = {"name": UsernameField}
 
     def __init__(self, *args, **kwargs):
