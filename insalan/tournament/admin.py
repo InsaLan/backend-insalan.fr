10a6ba3e1e9e71cfc78b5a866414edbab408a8c1
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2026-01-30 23:21:30 +0100

    add SwissRound name to admin list view

--- insalan/tournament/admin.py
+++ insalan/tournament/admin.py
@@ -2044,7 +2044,7 @@ class SwissSeedingInline(admin.TabularInline[SwissSeeding, SwissRound]):
 class SwissRoundAdmin(ModelAdmin):  # type: ignore
     """Admin handle for Swiss Round"""
 
-    list_display = ("id", "tournament")
+    list_display = ("id", "name", "tournament")
     search_fields = ["tournament"]
     inlines = [SwissSeedingInline]
     actions = ["create_swiss_matchs_action"]
